bc26070899146e3d583a5033320b0407aa899721
KaribelBT <karibel.barco@gmail.com>
2020-07-06 16:02:22 -0300

    queries sql

--- Clase49/queries.sql
+++ Clase49/queries.sql
@@ -1,9 +1,8 @@
-MY SQL
 
-/*CREAR TABLAS*/
+CREATE DATABASE bandas
 
 CREATE TABLE bandas(
-	id INT PRIMARY KEY AUTO_INCREMENT,
+	id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 	nombre VARCHAR (60)  NOT NULL,
     integrantes INT NOT NULL,
     fecha_inicio date NOT NULL,
@@ -12,23 +11,21 @@ CREATE TABLE bandas(
 )
 
 CREATE TABLE albumes(
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	nombre_album VARCHAR (60)  NOT NULL,
+	id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	nombre VARCHAR (60)  NOT NULL,
     duracion INT NOT NULL,
     banda INT NOT NULL,
     fecha_publicacion DATE NOT NULL
 )
 
 CREATE TABLE canciones(
-	id INT PRIMARY KEY AUTO_INCREMENT,
+	id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 	nombre VARCHAR (60)  NOT NULL,
     duracion INT NOT NULL,
     album INT NOT NULL,
     banda INT NOT NULL,
     fecha_publicacion DATE NOT NULL
 )
-
-/*INSERTAR DATOS*/
 
 INSERT INTO bandas
 VALUES (1, 'MAGO DE OZ', 9, '1988-01-01', NULL, 'ESPAÑA')
@@ -42,4 +39,26 @@ VALUES (1, 'GAIA III', 2, 1, '2010-04-06')
 INSERT INTO albumes
 VALUES (2, 'THE BETTER LIFE', 2, 2, '2000-02-08')
 
+INSERT INTO canciones
+VALUES (1, "Vodka 'n' roll", 4, 1, 1, '2010-04-06')
 
+INSERT INTO canciones
+VALUES (2, "Que el viento sople a tu favor", 5, 1, 1, '2010-04-06')
+
+INSERT INTO canciones
+VALUES (3, "Kryptonite", 4, 2, 2, '2000-02-08')
+
+INSERT INTO canciones
+VALUES (4, "Be Like That", 5, 2, 2, '2000-02-08')
+
+SELECT * FROM bandas
+
+SELECT * FROM bandas WHERE pais = "ESPAÑA"
+
+SELECT * FROM bandas WHERE integrantes = 1
+
+SELECT * FROM canciones WHERE fecha_publicacion >= 2016-01-01
+
+SELECT * FROM canciones WHERE duracion > 3
+
+SELECT * FROM albumes
